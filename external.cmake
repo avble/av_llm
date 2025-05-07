c154b404046e47db254e4c267c3ae2d73c20efa4
Harry <avble.harry@gmail.com>
2025-05-07 11:10:52 +0700

    add external

---
+++ external.cmake
@@ -0,0 +1,28 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    llama_cpp
+    GIT_REPOSITORY https://github.com/avble/llama.cpp.git
+    GIT_TAG        main
+)
+
+
+
+# FetchContent_Declare(
+#     av_connect
+#     GIT_REPOSITORY https://github.com/avble/av_connect.git
+#     GIT_TAG        main
+# )
+
+if(NOT llama_cpp_POPULATED)
+  option(LLAMA_CURL "llama libcur" ON)
+  option(LLAMA_BUILD_SERVER "llama server" ON)
+  option(LLAMA_BUILD_EXAMPLES "llama example" ON)
+  option(LLAMA_BUILD_COMMON "llama: build common utils library" ON)
+  FetchContent_Populate(llama_cpp)
+  add_subdirectory(${llama_cpp_SOURCE_DIR} ${llama_cpp_BINARY_DIR})
+endif()
+# if(NOT av_connect_POPULATED)
+#   FetchContent_Populate(av_connect)
+# endif()
+
