e67c583b1e1a1a62f53f8739aa06c3737ab3af50
Avble Harry <avble.harry@>
2025-06-14 22:06:28 +0700

    Update build script

--- build_sycl.bat
+++ build_sycl.bat
@@ -1,7 +1,3 @@
-call "E:\software_vs\VC\Auxiliary\Build\vcvars64.bat"
-REM set VS2022INSTALLDIR=E:\software_vs
-set VS2022INSTALLDIR=E:\software_vs\2022\BuildTools"
-call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat
 cmake -B build -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=icx ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DGGML_BACKEND_DL=OFF -DBUILD_SHARED_LIBS=ON ^
---
+++ build_sycl_prepare.bat
@@ -0,0 +1,4 @@
+call "E:\software_vs\VC\Auxiliary\Build\vcvars64.bat"
+REM set VS2022INSTALLDIR=E:\software_vs
+set VS2022INSTALLDIR=E:\software_vs\2022\BuildTools"
+call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat
