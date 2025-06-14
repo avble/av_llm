930b8aad7c025e5c61c64b713f8bd60d0db0e659
Avble Harry <avble.harry@>
2025-06-14 22:44:32 +0700

    Add build script

--- build_sycl.bat
+++ build_sycl.bat
@@ -1,7 +1,7 @@
-cmake -B build -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=icx ^
+cmake -B build_vulkan -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=icx ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DGGML_BACKEND_DL=OFF -DBUILD_SHARED_LIBS=ON ^
 -DGGML_SYCL=ON ^
 -DGGML_CPU=ON ^
 -DLLAMA_CURL=OFF -A x64
-cmake --build build --config=Release
+cmake --build_vulkan build --config=Release
