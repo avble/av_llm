62862eefe36e5df951b9505d06a25269e15dc9b4
Avble Harry <harry.avble@outlook.com>
2025-06-24 00:31:49 +0700

    Feat. fix build

--- build_sycl.bat
+++ build_sycl.bat
@@ -1,2 +1,2 @@
 cmake --preset x64-windows-sycl-release
-cmake --build build-x64-x64-windows-sycl-release
+cmake --build build-x64-windows-sycl-release
--- build_vulkan.bat
+++ build_vulkan.bat
@@ -1,2 +1,3 @@
+set Vulkan_SDK=C:\VulkanSDK\1.4.313.2
 cmake --preset x64-windows-vulkan-release
 cmake --build build-x64-windows-vulkan-release
