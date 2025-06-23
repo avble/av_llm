3b18408d3a181c06d44f8155aa82624963e9ff08
Harry <avble.harry@gmail.com>
2025-06-24 03:47:19 +0700

    Chore. Fix build windows

--- build_sycl.bat
+++ build_sycl.bat
@@ -1,2 +1,2 @@
-cmake --preset x64-windows-sycl-release
+cmake --preset x64-windows-sycl-release -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build build-x64-windows-sycl-release
--- build_vulkan.bat
+++ build_vulkan.bat
@@ -1,3 +1,3 @@
 set Vulkan_SDK=C:\VulkanSDK\1.4.313.2
-cmake --preset x64-windows-vulkan-release
+cmake --preset x64-windows-vulkan-release -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build build-x64-windows-vulkan-release
