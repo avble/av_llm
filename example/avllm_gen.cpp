48eb921c14faf3f8c8a0aa65460cc5cc8d7dd717
Avble Harry <harry.avble@outlook.com>
2025-07-09 11:00:40 +0700

    Task. improve example add qwen3's MCP

--- example/avllm_gen.cpp
+++ example/avllm_gen.cpp
@@ -37,6 +37,8 @@ static void print_usage(int, char ** argv)
 // global option
 std::string model_path;
 std::string prompt;
+int top_k   = 20;
+float top_p = 0.8;
 
 int main(int argc, char ** argv)
 {
@@ -133,7 +135,9 @@ int main(int argc, char ** argv)
         auto sparams         = llama_sampler_chain_default_params();
         sparams.no_perf      = false;
         llama_sampler * smpl = llama_sampler_chain_init(sparams);
-        llama_sampler_chain_add(smpl, llama_sampler_init_greedy());
+        llama_sampler_chain_add(smpl, llama_sampler_init_top_k(top_k));
+        llama_sampler_chain_add(smpl, llama_sampler_init_top_p(top_p, 20));
+        llama_sampler_chain_add(smpl, llama_sampler_init_dist(1234));
         return smpl;
     }();
     if (smpl == nullptr)
@@ -160,7 +164,7 @@ int main(int argc, char ** argv)
             return -1;
         }
 
-        if (true)
+        if (false)
         { // print token
             std::cout << "tokens: \n";
             for (auto token : prompt_tokens)
@@ -192,7 +196,7 @@ int main(int argc, char ** argv)
         const auto t_main_start   = ggml_time_us();
         llama_token new_token_id;
         int num_token = 0;
-        while (num_token++ < 100)
+        while (num_token++ < 500)
         {
             new_token_id = llama_sampler_sample(smpl, ctx, -1);
 
